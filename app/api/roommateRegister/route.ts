489b32c693ef4dbd3454e7a8d295a8a6b935bcd1
Simon Lee <misaeng.dev@gmail.com>
2023-05-31 13:47:38 -0400

    mod: api route modifed for dep

--- app/api/roommateRegister/route.ts
+++ app/api/roommateRegister/route.ts
@@ -5,29 +5,29 @@ export async function POST(request: Request) {
   const body = await request.json();
   const { name, email } = body;
 
-  const user = await prisma.roommateListing.create({
-    data: {
-      // userId String @db.ObjectId
-      // category String
-      // city String
-      // price Int
-      // roomType String
-      // moveDate DateTime?
-      // length String
-      // utility Boolean
-      // description String
-      // amenity String[]
-      // feature String[]
-      // imageSrc String[]
-      // ownerPre String[]
-      // roommatePre String[]
-      // coordinate Int[]
-      // contact String[]
-      // district String
-      // createdAt DateTime @default(now())
-      // updatedAt DateTime?
-    },
-  });
+  // const user = await prisma.roommateListing.create({
+  //   data: {
+  //     userId String @db.ObjectId
+  //     category String
+  //     city String
+  //     price Int
+  //     roomType String
+  //     moveDate DateTime?
+  //     length String
+  //     utility Boolean
+  //     description String
+  //     amenity String[]
+  //     feature String[]
+  //     imageSrc String[]
+  //     ownerPre String[]
+  //     roommatePre String[]
+  //     coordinate Int[]
+  //     contact String[]
+  //     district String
+  //     createdAt DateTime @default(now())
+  //     updatedAt DateTime?
+  //   },
+  // });
 
-  return NextResponse.json(user);
+  return NextResponse.json(body);
 }
