b863c77de5fde171f108feccd95b8553b7aa8d57
Simon Lee <misaeng.dev@gmail.com>
2023-06-13 19:22:32 -0400

    mod: image resizer modified

--- app/lib/imageResizer.ts
+++ app/lib/imageResizer.ts
@@ -42,14 +42,27 @@ export const resizeProfileImage = async (file: File): Promise<File> => {
 };
 
 export const resizeAdImage = async (file: File): Promise<File> => {
+  const MAX_WIDTH = 480; // Maximum width for resized image
+  const MAX_HEIGHT = 360; // Maximum height for resized image
+
   const srcCanvas = document.createElement('canvas');
   const dstCanvas = document.createElement('canvas');
-  const newWidth = 480;
-  const newHeight = 360;
 
   const img = new Image();
   img.src = URL.createObjectURL(file);
   await new Promise((resolve) => (img.onload = resolve));
+
+  let newWidth;
+  let newHeight;
+
+  // Calculate new dimensions that keep aspect ratio
+  if (img.width > img.height) {
+    newWidth = MAX_WIDTH;
+    newHeight = (img.height * newWidth) / img.width;
+  } else {
+    newHeight = MAX_HEIGHT;
+    newWidth = (img.width * newHeight) / img.height;
+  }
 
   srcCanvas.width = img.width;
   srcCanvas.height = img.height;
@@ -80,3 +93,43 @@ export const resizeAdImage = async (file: File): Promise<File> => {
     }, 'image/png');
   });
 };
+
+// export const resizeAdImage = async (file: File): Promise<File> => {
+//   const srcCanvas = document.createElement('canvas');
+//   const dstCanvas = document.createElement('canvas');
+//   const newWidth = 480;
+//   const newHeight = 360;
+
+//   const img = new Image();
+//   img.src = URL.createObjectURL(file);
+//   await new Promise((resolve) => (img.onload = resolve));
+
+//   srcCanvas.width = img.width;
+//   srcCanvas.height = img.height;
+//   srcCanvas.getContext('2d')?.drawImage(img, 0, 0);
+
+//   dstCanvas.width = newWidth;
+//   dstCanvas.height = newHeight;
+
+//   // Fill the background with white
+//   const ctx = dstCanvas.getContext('2d');
+//   if (ctx) {
+//     ctx.fillStyle = 'white';
+//     ctx.fillRect(0, 0, newWidth, newHeight);
+//   }
+
+//   await pica().resize(srcCanvas, dstCanvas);
+
+//   return new Promise((resolve) => {
+//     dstCanvas.toBlob((blob) => {
+//       if (blob) {
+//         const resizedFile = new File([blob], 'resized-image.png', {
+//           type: 'image/png',
+//         });
+//         resolve(resizedFile);
+//       } else {
+//         throw new Error('Failed to resize image');
+//       }
+//     }, 'image/png');
+//   });
+// };
