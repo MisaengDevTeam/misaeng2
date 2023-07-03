377dd36a97e2a3664fb71f6ebcdcec66326cbb73
Simon Lee <misaeng.dev@gmail.com>
2023-07-03 16:24:29 -0400

    dev: client form scroll to top added

--- app/client/form/page.tsx
+++ app/client/form/page.tsx
@@ -2,7 +2,7 @@
 
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const ClientForm = ({}) => {
   const [iframeHeight, setIframeHeight] = useState('4500');
@@ -25,6 +25,10 @@ const ClientForm = ({}) => {
     return () => window.removeEventListener('resize', updateHeight);
   }, []);
 
+  const handleScrollTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   if (!currentUser)
     return (
       <div className='flex flex-col items-center justify-center w-full py-8 my-8 gap-4'>
@@ -39,7 +43,7 @@ const ClientForm = ({}) => {
     );
   return (
     <div className='flex justify-center w-full py-8'>
-      <div className='w-full md:w-[50%]'>
+      <div className='flex flex-col justify-center items-center w-full md:w-[50%]'>
         <iframe
           src='https://docs.google.com/forms/d/e/1FAIpQLSfctRz8F0yAjLZcqZYb9I1d2uIsnGm13IVlTm8IlijpdmR6cQ/viewform?embedded=true'
           width='100%'
@@ -50,6 +54,14 @@ const ClientForm = ({}) => {
         >
           Loading…
         </iframe>
+        <div className='flex w-full justify-center'>
+          <button
+            onClick={handleScrollTop}
+            className='flex w-[80%] max-w-[360px] justify-center items-center bg-[#EC662A] text-[#FFF] rounded-full py-2 hover:shadow-xl'
+          >
+            맨위로 올라가기
+          </button>
+        </div>
       </div>
     </div>
   );
