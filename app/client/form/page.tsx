a7cec505766fc1747a477dfb6b6b86eb03fe406f
Simon Lee <misaeng.dev@gmail.com>
2023-07-03 11:33:11 -0400

    mod: google form height modified

--- app/client/form/page.tsx
+++ app/client/form/page.tsx
@@ -2,10 +2,29 @@
 
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 const ClientForm = ({}) => {
+  const [iframeHeight, setIframeHeight] = useState('4500');
   const { data: session } = useSession();
   const currentUser = session?.user;
+
+  useEffect(() => {
+    const updateHeight = () => {
+      if (window.innerWidth > 768) {
+        setIframeHeight('4100');
+      } else {
+        setIframeHeight('4500');
+      }
+    };
+
+    updateHeight();
+
+    window.addEventListener('resize', updateHeight);
+
+    return () => window.removeEventListener('resize', updateHeight);
+  }, []);
+
   if (!currentUser)
     return (
       <div className='flex flex-col items-center justify-center w-full py-8 my-8 gap-4'>
@@ -24,7 +43,7 @@ const ClientForm = ({}) => {
         <iframe
           src='https://docs.google.com/forms/d/e/1FAIpQLSfctRz8F0yAjLZcqZYb9I1d2uIsnGm13IVlTm8IlijpdmR6cQ/viewform?embedded=true'
           width='100%'
-          height='4100'
+          height={iframeHeight}
           frameBorder='0'
           marginHeight={0}
           marginWidth={0}
